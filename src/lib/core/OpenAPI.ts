909126c08e12a4356516db58063df7383b4625d1
KENMEUGNEMICHELE14 <michelekenmeugne@gmail.com>
2026-04-22 17:41:13 +0100

    ajout de la lib

--- src/lib/core/OpenAPI.ts
+++ src/lib/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'https://pre-hackverse-team-rodami.onrender.com/api',
+    BASE: 'https://pre-hackverse-team-rodami.onrender.com',
     VERSION: '1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
